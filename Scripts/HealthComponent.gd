a25c1246e756bd8e3c2532c1f651382bd5d41707
Timur <t.khafizov05@gmail.com>
2024-08-07 17:48:08 +0300

    Blank EnemyAttackArea prefab, blanked HealthComponent.gd

--- Scripts/HealthComponent.gd
+++ Scripts/HealthComponent.gd
@@ -1,15 +1,4 @@
 extends Node2D
 
-@export var maxHP = 10
-var currentHP
-
-
-func _ready():
-	currentHP = maxHP
-
-
-func takeDamage(damage):
-	currentHP -= damage
-	
-	if (currentHP <= 0):
-		get_parent().get_child(0).queue_free() #testing result
+func take_damage():
+	pass
