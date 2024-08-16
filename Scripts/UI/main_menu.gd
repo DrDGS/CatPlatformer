982ab111123686154a7afc101802c0377ba7e945
AntonenkoGr <gr_antonenkobox@mail.ru>
2024-08-16 14:31:38 +0300

    Language setting reflects on Main Menu

--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -1,10 +1,14 @@
 extends CanvasLayer
 
-var langCodes = ["en", "ru"]
 
 func _ready():
-	TranslationServer.set_locale("en")
+	TranslationServer.set_locale(Singleton.locale)
 
+
+func _process(delta):
+	TranslationServer.set_locale(Singleton.locale)
+	updateUI()
+	
 
 func _on_start_game_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Levels/testing_scene.tscn")
@@ -13,7 +17,12 @@ func _on_start_game_pressed():
 func _on_options_pressed():
 	get_tree().change_scene_to_file("res://Scenes/UI/options.tscn")
 	
-
+	
+func updateUI():
+	$MarginContainer/VBoxContainer/GameName.text = tr("GAME_NAME")
+	$MarginContainer/VBoxContainer/StartGame.text = tr("START_GAME")
+	$MarginContainer/VBoxContainer/Settings.text = tr("SETTINGS")
+	$MarginContainer/VBoxContainer/Exit.text = tr("EXIT")
 
 func _on_exit_pressed():
 	get_tree().quit()
--- Scripts/UI/settings.gd
+++ Scripts/UI/settings.gd
@@ -1,22 +1,31 @@
 extends CanvasLayer
 
 
+func _ready():
+	match Singleton.locale:
+		"en":
+			$MarginContainer/VBoxContainer/Language/selected = 0
+		"ru":
+			$MarginContainer/VBoxContainer/Language/selected = 1
+
+
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://Scenes/UI/main_menu.tscn")	
 
 
 func _on_language_item_selected(index):
 	match index:
-			0:
-				TranslationServer.set_locale("en")
-				updateUI()
-			1:
-				TranslationServer.set_locale("ru")
-				updateUI()
-				
+		0:
+			Singleton.locale = "en"
+			TranslationServer.set_locale(Singleton.locale)
+			updateUI()
+		1:
+			Singleton.locale = "ru"
+			TranslationServer.set_locale(Singleton.locale)
+			updateUI()
+
+
 func updateUI():
 	$MarginContainer/VBoxContainer/GameName.text = tr("SETTINGS")
 	$MarginContainer/VBoxContainer/Setting1.text = tr("SETTING_1")
 	$MarginContainer/VBoxContainer/Back.text = tr("BACKWARD")
-#	$(get_tree().get_root().get_node("MarginContainer/VBoxContainer/GameName")).text = tr("GAME_NAME")
-	
