291c4dc7a9fc217cdaa6f721c3af6743aaf93533
Григорий Антоненко <gr_antonenkobox@mail.ru>
2024-08-30 10:58:39 +0300

    Modified jump mechanic on physics view

--- Scripts/Character/Player.gd
+++ Scripts/Character/Player.gd
@@ -4,16 +4,32 @@ extends CharacterBody2D
 # VEL_RUN_COEF - multiplies SPEED to get running speed
 # ACCELERATION - delta for reaching speed
 # AC_STOP_COEF - multiplies ACCELERATION to get delta for dropping speed
-# JUMP_VELOCITY - vertical speed
 
 @export var SPEED = 100.0
 @export var VEL_RUN_COEF = 3
 @export var ACCELERATION = 30
 @export var AC_STOP_COEF = 1.25
-@export var JUMP_VELOCITY = -400.0
-@export var WALL_ClIMBING_VELOCITY = 25
+
+# Relating to JUMP Mechanic
+# JUMP_HEIGHT - height of peak
+# TIME_ASCEND - reaching peak time
+# TIME_DESCEND - falling on floor time
+# JUMP_VELOCITY - self-explanatory name
+# GRAVITY_ASC - gravity while ascending (~980 - approx project var)
+# GRAVITY_DSC - gravity while descending
+
+@export var JUMP_HEIGHT = -80
+@export var TIME_ASCEND = 0.4
+@export var TIME_DESCEND = 0.5
+
+@onready var JUMP_VELOCITY = (2 * JUMP_HEIGHT) / (TIME_ASCEND)
+@onready var GRAVITY_ASC = (-2 * JUMP_HEIGHT) / (TIME_ASCEND * TIME_ASCEND)
+@onready var GRAVITY_DSC = (-2 * JUMP_HEIGHT) / (TIME_DESCEND * TIME_DESCEND)
+
+
+@export var WALL_CLIMBING_VELOCITY = 25
 @export var WALL_KNOKBACK = 200
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 
 @export var hpComponent : Node2D
 @export var plInput : Node2D
@@ -30,18 +46,21 @@ func _ready():
 
 func _physics_process(delta):
 	apply_gravity(delta)
-	state_machine()
-	print("Player_state: ", State.keys()[player_state])
-
+	state_machine(delta)
+	print("Player state: ", State.keys()[player_state])
 	move_and_slide()
 
 
 func apply_gravity(delta):
-	if not (is_on_floor() or is_on_wall()):
-		velocity.y += gravity * delta 
+	velocity.y += get_gravity() * delta
+	print("Gravity: ", get_gravity())
+	
+
+func get_gravity():
+	return GRAVITY_ASC if velocity.y < 0.0 else GRAVITY_DSC
 
 
-func state_machine():
+func state_machine(delta):
 	match player_state:
 		State.Idle:
 			player_idle()
@@ -53,7 +72,8 @@ func state_machine():
 			player_run()
 			
 		State.Jump:
-			player_jump()
+			print("JUMP")
+			player_jump(delta)
 
 
 func player_idle():
@@ -89,16 +109,37 @@ func player_run():
 	if plInput.is_jump:
 		player_state = State.Jump
 		
-		
-func player_jump():
-	velocity.y = JUMP_VELOCITY
 
+func player_jump(delta):
+	velocity.y = JUMP_VELOCITY
+	
+	print("Vertical velocity: ", velocity.y)
+	
 	if is_on_floor() and not plInput.direction:
 		player_state = State.Idle
 	if is_on_floor() and plInput.direction and not plInput.is_run:
 		player_state = State.Walk
 	if is_on_floor() and plInput.direction and plInput.is_run:
 		player_state = State.Run
+	if plInput.is_jump and not is_on_floor():
+		pass
+
+
+func jump_animation_finished():
+	player_state = State.Idle
+
+
+func _process(_delta):
+	animation_logic(Vector2(plInput.direction, 0))
+	player_animation()
+	if (Input.is_action_just_pressed("TestTakeDamage")):
+		hpComponent.take_damage(1);
+
+
+func animation_logic(direction : Vector2):
+	animation_tree["parameters/blend_position"] = direction.normalized();
+	if direction.x != 0:
+		$Sprite2D.flip_h = direction.x < 0
 
 
 func player_animation():
@@ -111,65 +152,4 @@ func player_animation():
 			animation_player.play("CatWalk")
 		State.Jump:
 			animation_player.play("CatJump")
-
-
-func animation_logic(direction : Vector2):
-	animation_tree["parameters/blend_position"] = direction.normalized();
-	if direction.x != 0:
-		$Sprite2D.flip_h = direction.x < 0
-
-
-func _process(_delta):
-	animation_logic(Vector2(plInput.direction, 0))
-	player_animation()
-	if (Input.is_action_just_pressed("TestTakeDamage")):
-		hpComponent.take_damage(1);
-
-
-	#player_falling(delta)
-	#player_idle()
-	## Move
-	#player_walk(direction)
-	#if Input.is_action_pressed("run"):
-		#player_run(direction)
-	#
-	#player_jump(direction)
-##	player_climbing(delta)
-#
-	#print("State: ", State.keys()[current_state], " direction: ", direction)
-	#player_animation()
-	#
-#
-	#if is_on_wall() and not is_on_floor():
-		#is_wall_climbing = Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_left")
-	#else:
-		#is_wall_climbing = false
-	#if is_wall_climbing:
-		#var collision = get_slide_collision(0)
-		#var body = collision.get_collider()
-		#var wall_type = body.get_layer_for_body_rid(collision.get_collider_rid())
-		#if wall_type == 0:
-			#velocity.y += (WALL_ClIMBING_VELOCITY * delta)
-			#velocity.y = min(velocity.y, WALL_ClIMBING_VELOCITY)
-		#else:
-			#velocity.y = min(velocity.y, 0)
-			
-
-#func player_climbing(delta):
-	#if is_on_wall() and not is_on_floor():
-		#if (Input.is_action_pressed("move_right") 
-				#or Input.is_action_pressed("move_left")) :
-			#current_state = State.Climbing
-		#else:
-			#current_state = State.Idle
-			#
-	#if current_state == State.Climbing:
-		#var collision = get_slide_collision(0)
-		#var body = collision.get_collider()
-		#var wall_type = body.get_layer_for_body_rid(collision.get_collider_rid())
-		#if wall_type == 0:
-			#velocity.y += (WALL_ClIMBING_VELOCITY * delta)
-			#velocity.y = min(velocity.y, WALL_ClIMBING_VELOCITY)
-		#else:
-			#velocity.y = min(velocity.y, 0)
 
